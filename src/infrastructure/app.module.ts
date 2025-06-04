617fac501db3d69a352ed2e27468bd4ac3bf3f31
Nhan Tran <trannhan08101999@gmail.com>
2025-06-04 10:08:53 +0700

    minor refactor

--- src/infrastructure/app.module.ts
+++ src/infrastructure/app.module.ts
@@ -6,8 +6,8 @@ import { AuthGuard } from './presentation/rest/guards/auth.guard';
 import { HttpExceptionFilter } from './presentation/rest/middleware/exception-filter.middleware';
 import { RestApiModule } from './presentation/rest/restApi.module';
 
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { ClassSerializerInterceptor, Module, ValidationPipe } from '@nestjs/common';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [InMemoryDatabaseModule, RestApiModule, CryptoModule, EmailModule, JwtModule],
@@ -16,6 +16,7 @@ import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
     { provide: APP_GUARD, useClass: AuthGuard },
     { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    { provide: APP_PIPE, useClass: ValidationPipe },
   ],
 })
 export class AppModule {}
--- src/shared/types/common-type.d.ts
+++ src/shared/types/common-type.d.ts
@@ -1,16 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
 type Id = number | string;
+type ObjectLiteral = Record<string, any>;
+
+/** Maping types */
 type Maybe<T> = T | null | undefined;
 type Nullable<T> = T | null;
-type NullPartial<T> = {
-  [P in keyof T]?: Maybe<T[P]>;
-};
-type IsUndefined<T> = Exclude<T, undefined> extends never ? true : false;
-
+type NullPartial<T> = { [K in keyof T]?: Maybe<T[K]> };
 type ClassType<T> = { new (...args: any[]): T };
-type Prettier<T> = { [K in keyof T]: T[K] } & {};
-type CompactObj<T> = { [K in keyof T as IsUndefined<T[K]> extends true ? never : K]: T[K] };
 
-type FunctionLike<Params = unknown[], Response = unknown> = (...args: Params) => Response;
+/** Boolean condition type */
+type IsAsignableBy<T, K> = Exclude<T, K> extends never ? true : false;
+type FunctionLike<Params = unknown[], Result = unknown> = (...args: Params) => Result;
 type ObjectLike<T> = Pick<T, { [K in keyof T]: T[K] extends FunctionLike ? never : K }[keyof T]>;
+
+/** Misc */
+type Prettier<T> = { [K in keyof T]: T[K] } & {};
+type CompactObj<T> = { [K in keyof T as IsAsignableBy<T[K], undefined> extends true ? never : K]: T[K] };
